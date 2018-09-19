b9535387cf7c88d98518c9b24dfc3da18cd45cea
Erik Kee <erik.chkee@gmail.com>
2018-09-18 23:04:52 -0500

    debug04

--- js/script.js
+++ js/script.js
@@ -10,22 +10,22 @@ $(document).ready(function() {
 
   snd1.appendChild(src1);
 
-  // var snd2 = new Audio();
-  // var src2 = document.createElement('source');
-  // src2.type = 'audio/mpeg';
-  // src2.src = './src/audio/karanokokoro-ng.mp3';
-  // src2.preload = 'auto';
-  // snd2.oncanplay = function() {
-  //   canPlayLiao('src2');
-  // };
-  // snd2.appendChild(src2);
+  var snd2 = new Audio();
+  var src2 = document.createElement('source');
+  src2.type = 'audio/mpeg';
+  src2.src = './src/audio/karanokokoro-ng.mp3';
+  src2.preload = 'auto';
+  snd2.oncanplay = function() {
+    canPlayLiao('src2');
+  };
+  snd2.appendChild(src2);
 
   var audio1Ready = false;
   var audio2Ready = false;
   var bothReady = false;
 
   snd1.volume = 1;
-  // snd2.volume = 0;
+  snd2.volume = 0;
 
   var playWithGuitar = true;
 
@@ -38,20 +38,20 @@ $(document).ready(function() {
       console.log('src1 ready');
       audio1Ready = true;
       bothReady = true;
-    } /* else if (src === 'src2') {
+    } else if (src === 'src2') {
       console.log('src2 ready');
       audio2Ready = true;
-    } */
+    }
 
-    /* if (audio1Ready && audio2Ready) {
+    if (audio1Ready && audio2Ready) {
       console.log('BOTH READY');
       bothReady = true;
       $('#play-button').css('opacity', '1');
     } else {
       $('#play-button').css('opacity', '0.3');
-    } */
+    }
 
-    $('#debug').html("src1 = " + audio1Ready + " | src2 = " /* + audio2Ready */ + " | both = " + bothReady);
+    $('#debug').html("src1 = " + audio1Ready + " | src2 = " + audio2Ready + " | both = " + bothReady);
   }
 
 
@@ -67,13 +67,13 @@ $(document).ready(function() {
       if (snd1.duration > 0 && !snd1.paused) {
         $('#play-button').html('PLAY');
         snd1.load();
-        //snd2.load();
+        snd2.load();
         $('#debug2').html("PLAY PRESSED - PLAY");
       } else {
         $('#play-button').html('STOP');
         $('#debug2').html("PLAY PRESSED - STOP");
         snd1.play();
-        //snd2.play();
+        snd2.play();
       }
     }
   });
@@ -83,12 +83,12 @@ $(document).ready(function() {
     if (playWithGuitar === true) {
       playWithGuitar = false;
       snd1.volume = 0;
-      //snd2.volume = 0.9;
+      snd2.volume = 0.9;
       $('#switch-button').html('SWITCH - NO GUITAR');
     } else {
       playWithGuitar = true;
       snd1.volume = 1;
-      //snd2.volume = 0;
+      snd2.volume = 0;
       $('#switch-button').html('SWITCH - WITH GUITAR');
     }
   });
